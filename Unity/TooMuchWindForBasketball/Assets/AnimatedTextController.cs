a782845f5f9d97137a34eb69ff796e11aa9f9f24
Fernando Guillen <fguillen.mail@gmail.com>
2021-01-31 14:12:39 +0100

    Working in the HowToPlay slideshow

---
+++ Unity/TooMuchWindForBasketball/Assets/AnimatedTextController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedTextController : MonoBehaviour
+{
+    Animator animator;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        Invoke("StartAnimation", Random.Range(0f, 0.5f));
+    }
+
+    void StartAnimation()
+    {
+        animator.SetTrigger("startMoving");
+    }
+}
